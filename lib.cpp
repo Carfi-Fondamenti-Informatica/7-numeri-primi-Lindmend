3d8a4a2833356e32f6aaf460855fa7d1512f704a
Lindmend <91802623+Lindmend@users.noreply.github.com>
2021-12-01 17:29:32 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+bool numeroPrimo(int a){
+    bool primo = true;
+    for (int b=2; b<a; b++){
+        int risultato = a % b;
+        if (risultato==0 and b<a){
+            primo = false;
+            break;
+
+        }
+    }
+    return primo;
+}
